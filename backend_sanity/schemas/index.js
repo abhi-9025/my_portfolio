5286c2c2c2c2aa975519dbcad79486529fa79816
Abhishek Bharti <abhishek@Abhisheks-MacBook-Air.local>
2023-07-05 22:27:17 +0530

    added all the required schemsa

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,2 +1,9 @@
-import testimonials from "./testimonials"
-export const schemaTypes = [ testimonials]
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+export const schemaTypes = [testimonials, experiences,workExperience,contact, skills, works, brands, abouts]
