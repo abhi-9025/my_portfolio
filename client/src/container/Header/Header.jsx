23790ec15fe05d56197e464e1266ab23e9bdcbd0
Abhishek Bharti <abhishek@Abhisheks-MacBook-Air.local>
2023-07-14 20:26:17 +0530

    using HOC to customise components

--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
+import { AppWrap } from '../../wrapper';
 
 const scaleVariants = {
   whileInView: {
@@ -65,4 +66,4 @@ const Header = () => (
     </motion.div>
   </div>
 );
-export default Header
+export default  AppWrap(Header,'home')
