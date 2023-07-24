f59d17ddd7afadb6f3f17556b13cabf696aa9a74
Abhishek Bharti <abhishek@Abhisheks-MacBook-Air.local>
2023-07-24 17:43:23 +0530

    added links to social media platforms

--- client/src/components/SocialMedia.jsx
+++ client/src/components/SocialMedia.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import {BsTwitter,BsInstagram} from 'react-icons/bs'
-import {FaFacebookF} from 'react-icons/fa'
+import React from "react";
+import { BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-        <div>
-            <BsTwitter/>
-        </div>
-        <div>
-            <BsInstagram/>
-        </div>
-        <div>
-            <FaFacebookF/>
-        </div>
+    <div className="app__social">
+      <div>
+        <a
+          href="https://www.linkedin.com/in/abhishek-bharti-937467197/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
+      <div>
+        <a href="https://twitter.com/Abhishe14569386" target="_blank">
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.instagram.com/abhi_.25/" target="_blank">
+          <BsInstagram />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
