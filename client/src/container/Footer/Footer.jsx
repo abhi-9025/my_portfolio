f7018ddb9e94e963ac9a5fc580db718046d410af
Abhishek Bharti <abhishek@Abhisheks-MacBook-Air.local>
2023-07-24 14:42:41 +0530

    minor fixes

--- client/src/container/Footer/Footer.jsx
+++ client/src/container/Footer/Footer.jsx
@@ -42,11 +42,11 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
+          <a href="mailto:abhisaroj192001@gmail.com" className="p-text">abhisaroj192001@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
+          <a href="tel:+91 (885) 390-2534" className="p-text">+91 (885) 390-2534</a>
         </div>
       </div>
       {!isFormSubmitted ? (
